9ac2c22c59dc916ea0f5d117c1f1e5f9fadc780c
camocreepercast <47070087+camocreepercast@users.noreply.github.com>
2020-07-10 19:35:04 -0400

    a

---
+++ Assets/Cue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour
+{
+	public PlayerMovement PM;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (PM.canHitBall)
+		{
+			Rigidbody2D rb = other.attachedRigidbody;
+			Vector3 forceDir = transform.up;
+			rb.AddForce(forceDir * 2f,ForceMode2D.Impulse);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,14 @@ public class PlayerMovement : MonoBehaviour
 	Camera mainCam;
 	public float moveSpeed = 1;
 
-	public bool FollowMouse = true;
+	public GameObject cue;
+	public float cuePushAmount = 1;
+	bool isPushingCue = false;
+	[HideInInspector]
+	public bool canHitBall = false;
+	public float extendedCooldown;
+	public float retractedCooldown;
+
 
 	Vector2 userInput = Vector2.zero;
 	Vector2 mousePosition = Vector2.zero;
@@ -19,27 +26,45 @@ public class PlayerMovement : MonoBehaviour
 
 	void Update()
 	{
-		userInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+		userInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+		transform.GetChild(0).up = direction;
+
+		if (Input.GetMouseButtonDown(0)&&!isPushingCue)
+		{
+			StartCoroutine(ShootCue());
+		}
 	}
 
 	void FixedUpdate()
 	{
-		if (Vector2.Distance(transform.position, mousePosition) > .1f)
-			transform.Translate(-userInput * moveSpeed * Time.fixedDeltaTime);
+		transform.Translate(userInput * moveSpeed);
 
-		Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+		
 
-		if (FollowMouse)
+	}
+
+	IEnumerator ShootCue()
+	{
+		isPushingCue = true;
+		canHitBall = true;
+		for (int i = 0; i < cuePushAmount; i++)
 		{
-			transform.up = -direction;
-			transform.GetChild(0).up = transform.up;
+			yield return null;
+			cue.transform.Translate(Vector2.up * .01f);
 		}
-		else
+		canHitBall = false;
+		yield return new WaitForSeconds(extendedCooldown); //hold it out for a bit
+		for (int i = 0; i < cuePushAmount; i++)
 		{
-			transform.GetChild(0).up = direction;
-			transform.up = Vector2.up;
+			yield return null;
+			yield return null;
+			cue.transform.Translate(Vector2.up * -.01f);
 		}
 
+		yield return new WaitForSeconds(retractedCooldown); //cooldown
+		isPushingCue = false;
 	}
 }

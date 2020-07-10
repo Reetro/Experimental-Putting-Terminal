691d8773c9c15fd323dba5b62618e1c2d38ba63b
camocreepercast <47070087+camocreepercast@users.noreply.github.com>
2020-07-10 16:33:43 -0400

    PlayerMovement
    Added movement script and some random sprites to test the velocity

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	Camera mainCam;
+	[HideInInspector]
+	public Rigidbody2D rb;
+	public float movementSpeed;
+	public float maxFallingVelocity;
+
+	float input = 0;
+
+	void Awake()
+	{
+		mainCam = Camera.main;
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		input = Input.GetAxisRaw("Horizontal");
+		if (rb.velocity.y < -maxFallingVelocity)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, -maxFallingVelocity);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce((Vector2.right * movementSpeed * input * Time.fixedDeltaTime),ForceMode2D.Impulse);
+	}
+}

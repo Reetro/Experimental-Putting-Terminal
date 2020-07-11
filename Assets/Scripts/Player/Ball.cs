bbdb27e1d44ab8d5e53d52ac8ab2d8d78379ad4c
Reetro <wells.justin70@yahoo.com>
2020-07-11 11:38:39 -0600

    gameplay prototype

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -5,14 +5,21 @@ public class Ball : MonoBehaviour
     private Rigidbody2D myRigidBody2d = null;
     private Vector2 currentVelocity;
     
-    void Start()
+    void Awake()
     {
         myRigidBody2d = GetComponent<Rigidbody2D>();
+
+        print(myRigidBody2d);
     }
 
     private void FixedUpdate()
     {
         currentVelocity = myRigidBody2d.velocity;
+    }
+
+    public void ThrowBall(float speed)
+    {
+        myRigidBody2d.velocity = transform.up * speed;
     }
 
     public Vector2 GetCurrentVelocity()
---
+++ Assets/Scripts/Player/StaticPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class StaticPlayer : MonoBehaviour
+{
+    Camera mainCam;
+    Vector2 mousePosition = Vector2.zero;
+
+    public GameObject ballPrefab;
+    public Transform spawnTransform;
+    public int playerBallCount = 1;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+
+    void Update()
+    {
+        mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.GetChild(0).up = direction;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnBall();
+        }
+    }
+
+    public void SpawnBall()
+    {
+        bool cantSpawn = playerBallCount <= 0;
+
+        if (!cantSpawn)
+        {
+            var ball = Instantiate(ballPrefab, (Vector2)spawnTransform.position, spawnTransform.rotation);
+
+            ball.GetComponent<Ball>().ThrowBall(10);
+
+            playerBallCount--;
+        }
+    }
+}

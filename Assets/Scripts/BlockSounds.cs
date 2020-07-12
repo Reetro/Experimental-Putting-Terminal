854d38fc01c71bb8feec1eecf107474f8f3cebaf
Reetro <wells.justin70@yahoo.com>
2020-07-12 10:10:40 -0600

    Implemented wall sounds effects

---
+++ Assets/Scripts/BlockSounds.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BlockSounds : ObstalcleBase
+{
+    [SerializeField] AudioClip[] blockSounds = null;
+    private AudioSource myAudioSorce;
+
+    private void Awake()
+    {
+        myAudioSorce = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (IsObjectBall(collision.gameObject))
+        {
+            PlaySound();
+        }
+    }
+
+    private void PlaySound()
+    {
+        AudioClip clip = blockSounds[Random.Range(0, blockSounds.Length)];
+
+        myAudioSorce.PlayOneShot(clip);
+    }
+}
